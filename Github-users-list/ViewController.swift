95fe3e1e4f8b8ff72dd86d727bf113f23f900c3b
egor.soloviev <egor.soloviev@acronis.com>
2022-03-20 14:37:15 +0300

    Initial project setup

---
+++ Github-users-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github-users-list
+//
+//  Created by Egor Solovev on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
